c3ab13f3c71eb66e5ecd4f5d3d09e7d30a7a5b8f
richiiie94 <47447885+richiiie94@users.noreply.github.com>
2021-06-20 23:39:17 +0700

    update 1

---
+++ wedding-invitation/src/app/app-routing.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  // { path: '**', component: PageNotFoundComponent }
+
+  {
+    path: 'home',
+    component: HomeComponent,
+    data: {
+      title: 'Home',
+    }
+  },
+
+  {
+    path: 'login',
+    component: LoginComponent,
+    data: {
+      title: 'Login',
+    }
+  },
+
+  {
+    path: 'register',
+    component: RegisterComponent,
+    data: {
+      title: 'Register',
+    }
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {enableTracing: true})],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
