b370fc4ee1c855df11e9ab733c7255a4d69b861f
richiiie94 <47447885+richiiie94@users.noreply.github.com>
2022-05-03 17:21:12 +0700

    update to scss

--- react-trial/src/index.js
+++ react-trial/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+import './styles.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
