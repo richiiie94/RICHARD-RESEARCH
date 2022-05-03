8c82c5b1be2d49e66d9abc4913ffbd9a1fd2eab7
richiiie94 <47447885+richiiie94@users.noreply.github.com>
2022-05-03 18:46:21 +0700

    navbar

--- react-trial/src/App.js
+++ react-trial/src/App.js
@@ -1,31 +1,36 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.scss';
+import NavigationBar from './components/NavigationBar';
+import './styles.scss'
 
 function App() {
   return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload. 
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //     <div class="mb-3">
+    //       <label for="exampleFormControlInput1" class="form-label">Email address </label>
+    //       <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+    //     </div>
+    //     <div class="mb-3">
+    //       <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+    //       <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    //     </div>
+    //   </header>
+    // </div>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React :smile:
-        </a>
-        <div class="mb-3">
-          <label for="exampleFormControlInput1" class="form-label">Email address</label>
-          <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
-        </div>
-        <div class="mb-3">
-          <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-          <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-        </div>
-      </header>
+      <NavigationBar />
     </div>
   );
 }
