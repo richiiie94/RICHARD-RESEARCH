b53b9601a532b393322f437812ab959b25d34e30
richiiie94 <47447885+richiiie94@users.noreply.github.com>
2022-05-15 21:03:14 +0700

    NavBar, Home

--- react-trial/src/components/Home.js
+++ react-trial/src/components/Home.js
@@ -1,19 +1,38 @@
-
+import React from 'react';
 import '../styles.scss'
 import '../styles/LandingPage.scss'
 
-function Home () {
-    return (
-        <div>
-            <div className="container">
-                <div className="row">
-                    <div className="col align-self-center homeTitle">
-                        COCONUT
+class Home extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="container">
+                    <div className="row whiteColor">
+                        <div className="col text-start">
+                            <div className="row leftHomeTitle">
+                                COCONUT
+                            </div>
+                            <div className="row leftHomeSubtitle">
+                                <div className="row">
+                                    co·co·nut
+                                </div>
+                                <div className="row">
+                                    /ˈkōkəˌnət/
+                                </div>
+                                <div className="row justifyText">
+                                    the large, oval, brown seed of a tropical palm, consisting of a hard shell lined with edible white flesh and containing a clear liquid. It grows inside a woody husk, surrounded by fiber.
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col text-end rightHomeTitle">
+                            COCONUT
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Home;
--- react-trial/src/components/NavigationBar.js
+++ react-trial/src/components/NavigationBar.js
@@ -1,33 +1,36 @@
 
+import React from 'react';
 import '../styles.scss'
 import '../styles/LandingPage.scss'
 
-function NavigationBar () {
-    return (
-        <div>
-            <nav className="navbar navbar-expand-lg navbar-light">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Navbar</a>
+class NavigationBar extends React.Component {
+    render() {
+        return (
+            <div>
+                <nav className="navbar navbar-expand-lg navbar-light">
+                    <div className="container-fluid">
+                        <a className="navbar-brand" href="#">Navbar</a>
 
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
 
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                                <li className="nav-item">
+                                    <a className="nav-link active" aria-current="page" href="#">Home</a>
+                                </li>
 
-                            <li className="nav-item">
-                                <a className="nav-link active" href="#">About</a>
-                            </li>
-                        </ul>
+                                <li className="nav-item">
+                                    <a className="nav-link active" href="#">About</a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
-            </nav>
-        </div>
-    );
+                </nav>
+            </div>
+        );
+    }
 }
 
 export default NavigationBar;
