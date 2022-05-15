26b8a357ee80adfa56bd2e03ed6960c40d430cf5
richiiie94 <47447885+richiiie94@users.noreply.github.com>
2022-05-15 19:16:29 +0700

    background

--- react-trial/src/App.js
+++ react-trial/src/App.js
@@ -1,38 +1,36 @@
 // import logo from './logo.svg';
-import './App.scss';
-import NavigationBar from './components/NavigationBar';
 import './styles.scss'
+import './styles/LandingPage.scss'
+import './App.scss';
 
-function App() {
-  return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload. 
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //     <div class="mb-3">
-    //       <label for="exampleFormControlInput1" class="form-label">Email address </label>
-    //       <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
-    //     </div>
-    //     <div class="mb-3">
-    //       <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-    //       <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-    //     </div>
-    //   </header>
-    // </div>
-    <div className="App">
-      <NavigationBar />
-    </div>
-  );
+import React from 'react';
+import Home from './components/Home';
+import NavigationBar from './components/NavigationBar';
+
+import coconutImage from './assets/images/coconut.jpg';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {imageURL: coconutImage};
+  }
+
+  handleImageChange() {
+    this.setState({
+      imageURL: coconutImage
+    })
+  }
+  
+  render() {
+    return (
+      <div>
+        <div className="firstBG" style={{backgroundImage: `url(${this.state.imageURL})`}}>
+          <NavigationBar />
+          <Home />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ react-trial/src/components/Home.js
@@ -0,0 +1,19 @@
+
+import '../styles.scss'
+import '../styles/LandingPage.scss'
+
+function Home () {
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col align-self-center homeTitle">
+                        COCONUT
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
