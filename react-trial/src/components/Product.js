8c8bf0e5d6418883886531dc2dbea1b570f5e266
richiiie94 <47447885+richiiie94@users.noreply.github.com>
2022-05-23 08:38:18 +0700

    update

--- react-trial/src/components/Product.js
+++ react-trial/src/components/Product.js
@@ -9,15 +9,15 @@ class Product extends React.Component {
     render() {
         return (
             <div>
-                <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+                <div id="carouselProduct" className="carousel carousel-dark carousel-fade" data-bs-ride="carousel">
                     <div className="carousel-indicators">
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        <button type="button" data-bs-target="#carouselProduct" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselProduct" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselProduct" data-bs-slide-to="2" aria-label="Slide 3"></button>
                     </div>
                     
                     <div className="carousel-inner">
-                        <div className="carousel-item active">
+                        <div className="carousel-item active" data-bs-interval="8000">
                             <div className="row">
                                 <div className="col">
                                     <img src={product1} className="d-block w-100" alt="product1" />
@@ -28,10 +28,12 @@ class Product extends React.Component {
                                 </div>
                             </div>
                         </div>
-                        <div className="carousel-item">
+
+                        <div className="carousel-item" data-bs-interval="8000">
                             <img src={product2} className="d-block w-100" alt="product2" />
                         </div>
-                        <div className="carousel-item">
+
+                        <div className="carousel-item" data-bs-interval="8000">
                             <img src={product3} className="d-block w-100" alt="product3" />
                         </div>
                     </div>
