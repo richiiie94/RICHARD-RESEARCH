8d513f9a9b4238c5762ba6809dbf36b75bd29d0d
richiiie94 <47447885+richiiie94@users.noreply.github.com>
2022-05-28 14:38:51 +0700

    home responsive

--- react-trial/src/components/Home.js
+++ react-trial/src/components/Home.js
@@ -8,28 +8,24 @@ class Home extends React.Component {
             <div>
                 <div className="container">
                     <div className="row whiteColorHome">
-                        <div className="col">
+                        <div className="col-sm-6 px-4 py-2 py-sm-4">
                             <div className="row leftHomeTitle">
                                 COCONUT
                             </div>
-                            <div className="row leftHomeSubtitle">
-                                <div className="row">
-                                    co·co·nut
-                                </div>
-                                <div className="row">
-                                    /ˈkōkəˌnət/
-                                </div>
-                                <div className="row justifyText">
-                                    the large, oval, brown seed of a tropical palm, consisting of a hard shell lined with edible white flesh and containing a clear liquid. It grows inside a woody husk, surrounded by fiber.
-                                </div>
+                            <div className="row leftHomeSubtitle justifyText">
+                                co·co·nut
+                                <br></br>
+                                /ˈkōkəˌnət/
+                                <br></br>
+                                the large, oval, brown seed of a tropical palm, consisting of a hard shell lined with edible white flesh and containing a clear liquid. It grows inside a woody husk, surrounded by fiber.
                             </div>
                         </div>
 
-                        <div className="col">
-                            <div className="row text-end rightHomeTitle">
+                        <div className="col-sm-6 px-4 py-2 py-sm-4">
+                            <div className="row rightHomeTitle">
                                 LOREM IPSUM
                             </div>
-                            <div className="row text-end rightHomeSubtitle justifyText">
+                            <div className="row rightHomeSubtitle justifyText">
                                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                             </div>
                         </div>
